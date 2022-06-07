c77cea1c6e7e00efe0d75d545639cd7a42b417f1
Teyeb <hussenab@gmail.com>
2022-06-07 21:15:08 +0300

    app.js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,70 @@
 import React, {useState, useEffect} from 'react'
-import { Table } from 'react-bootstrap';
+import { useNavigate } from "react-router-dom";
+import { Table, Form, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import axios from 'axios';
-function App() {
-  const [posts, setPosts] = useState([])
 
+
+function App() {
+  
+  const [posts, setPosts] = useState([])
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  
   useEffect(() => {
     axios.get('http://localhost:3001/posts')
   .then(function(data){
-    console.log(data.data)
+    
     setPosts(data.data)
   })
   .catch(err => console.log(err))
   }, [])
   
+  const handleSubmit = () => {
+    const post = {
+      id: new Date(),
+      title,
+      author
+    }
+    axios.post('http://localhost:3001/posts', post)
+    .then(response => console.log('response', response))
+    .catch(error => console.log(error, 'error '))
+  }
+  const handleDelete = (id) => {
+    
+    axios.delete(`http://localhost:3001/posts/${id}`)
+    .then(function(response){
+      console.log(response)
+    })
+    .catch(error => console.log(error, 'error '))
+  }
   return (
     <div className="Alpp">
       <header className="App-header">
         <h1>Firebase - hosted app </h1>
       </header>
       <main className='container mx-auto'>
-         <p>
-          <button className="btn btn-outline-primary mt-4" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-            Create Post
-          </button>
-          </p>
-        <div className="collapse" id="collapseExample">
-          <div className="card card-body">
-            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
-          </div>
-        </div>
+         
+      <Form className=' mx-auto mt-3 mb-3'>
+          <Form.Group className="mb-3" controlId="formBasicTitle">
+            <Form.Label>Title</Form.Label>
+            <Form.Control type="Title" onChange={(e) => setTitle(e.target.value)} placeholder="Enter Title" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicAuthor">
+            <Form.Label>Author</Form.Label>
+            <Form.Control type="Author" onChange={(e) => setAuthor(e.target.value)} placeholder="Author" />
+          </Form.Group>
+          
+          <Button variant="primary" onClick={handleSubmit} type="submit">
+            Submit
+          </Button>
+      </Form>
+              
       <Table striped bordered hover >
         <thead>
-          <tr>
+          <tr className='bg-dark text-white'>
             <th>Title </th>
             <th>Author</th>
             <th>Action</th>
@@ -47,7 +78,9 @@ function App() {
                 <td>{post.title}</td>
                 <td>{post.author}</td>
                 <td><button className='btn btn-outline-info'>Edit</button></td>
-                <td><button className='btn btn-outline-danger'>Delete</button></td>
+                <td><button className='btn btn-outline-danger' onClick={function(){
+                  handleDelete(post.id)
+                }}>Delete</button></td>
               </tr>
             ) 
           }
